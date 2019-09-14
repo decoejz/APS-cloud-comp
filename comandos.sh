914e15c0ae11e00e03d28c0b6fea665db574b4e3
decoejz <decoejz@gmail.com>
2019-09-14 00:31:59 -0300

    organizando web server

--- comandos.sh
+++ comandos.sh
@@ -1,4 +1,4 @@
-sudo apt upgrade
+sudo apt update
 sudo apt install python3
 sudo apt install python3-pip
 pip3 install flask
--- webServer.py
+++ webServer.py
@@ -29,4 +29,4 @@ def checkStatus():
     return ('200')
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True,host='0.0.0.0')
